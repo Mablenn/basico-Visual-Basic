5b81c6f19d08b241eaebea3bf59d7cfeb585b893
Mablenn <marcos.broseta@gmail.com>
2023-07-21 11:50:29 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SqlSelectCommand1 = New System.Data.SqlClient.SqlCommand()
+        Me.SqlConnection1 = New System.Data.SqlClient.SqlConnection()
+        Me.SqlInsertCommand1 = New System.Data.SqlClient.SqlCommand()
+        Me.SqlDataAdapter1 = New System.Data.SqlClient.SqlDataAdapter()
+        Me.DataSet11 = New TestClientDatabase.DataSet1()
+        Me.DataSet21 = New TestClientDatabase.DataSet2()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AnyoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AnuncianteDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataSet11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataSet21, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SqlSelectCommand1
+        '
+        Me.SqlSelectCommand1.CommandText = "SELECT ClienteDatabase.*" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "FROM   ClienteDatabase"
+        Me.SqlSelectCommand1.Connection = Me.SqlConnection1
+        '
+        'SqlConnection1
+        '
+        Me.SqlConnection1.ConnectionString = "Data Source=MAB-PORTATIL\SQLEXPRESS;Initial Catalog=ClientDB;Integrated Security=" &
+    "True;Pooling=False"
+        Me.SqlConnection1.FireInfoMessageEventOnUserErrors = False
+        '
+        'SqlInsertCommand1
+        '
+        Me.SqlInsertCommand1.CommandText = "INSERT INTO [ClienteDatabase] ([Id], [Anyo], [Anunciante]) VALUES (@Id, @Anyo, @A" &
+    "nunciante)"
+        Me.SqlInsertCommand1.Connection = Me.SqlConnection1
+        Me.SqlInsertCommand1.Parameters.AddRange(New System.Data.SqlClient.SqlParameter() {New System.Data.SqlClient.SqlParameter("@Id", System.Data.SqlDbType.Int, 0, "Id"), New System.Data.SqlClient.SqlParameter("@Anyo", System.Data.SqlDbType.NChar, 0, "Anyo"), New System.Data.SqlClient.SqlParameter("@Anunciante", System.Data.SqlDbType.Int, 0, "Anunciante")})
+        '
+        'SqlDataAdapter1
+        '
+        Me.SqlDataAdapter1.InsertCommand = Me.SqlInsertCommand1
+        Me.SqlDataAdapter1.SelectCommand = Me.SqlSelectCommand1
+        Me.SqlDataAdapter1.TableMappings.AddRange(New System.Data.Common.DataTableMapping() {New System.Data.Common.DataTableMapping("Table", "ClienteDatabase", New System.Data.Common.DataColumnMapping() {New System.Data.Common.DataColumnMapping("Id", "Id"), New System.Data.Common.DataColumnMapping("Anyo", "Anyo"), New System.Data.Common.DataColumnMapping("Anunciante", "Anunciante")})})
+        '
+        'DataSet11
+        '
+        Me.DataSet11.DataSetName = "DataSet1"
+        Me.DataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'DataSet21
+        '
+        Me.DataSet21.DataSetName = "DataSet2"
+        Me.DataSet21.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.AnyoDataGridViewTextBoxColumn, Me.AnuncianteDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataMember = "ClienteDatabase"
+        Me.DataGridView1.DataSource = Me.DataSet21
+        Me.DataGridView1.Location = New System.Drawing.Point(53, 36)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(877, 385)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'IdDataGridViewTextBoxColumn
+        '
+        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "Id"
+        Me.IdDataGridViewTextBoxColumn.HeaderText = "Id"
+        Me.IdDataGridViewTextBoxColumn.MinimumWidth = 8
+        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        Me.IdDataGridViewTextBoxColumn.Width = 150
+        '
+        'AnyoDataGridViewTextBoxColumn
+        '
+        Me.AnyoDataGridViewTextBoxColumn.DataPropertyName = "Anyo"
+        Me.AnyoDataGridViewTextBoxColumn.HeaderText = "Anyo"
+        Me.AnyoDataGridViewTextBoxColumn.MinimumWidth = 8
+        Me.AnyoDataGridViewTextBoxColumn.Name = "AnyoDataGridViewTextBoxColumn"
+        Me.AnyoDataGridViewTextBoxColumn.Width = 150
+        '
+        'AnuncianteDataGridViewTextBoxColumn
+        '
+        Me.AnuncianteDataGridViewTextBoxColumn.DataPropertyName = "Anunciante"
+        Me.AnuncianteDataGridViewTextBoxColumn.HeaderText = "Anunciante"
+        Me.AnuncianteDataGridViewTextBoxColumn.MinimumWidth = 8
+        Me.AnuncianteDataGridViewTextBoxColumn.Name = "AnuncianteDataGridViewTextBoxColumn"
+        Me.AnuncianteDataGridViewTextBoxColumn.Width = 150
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(978, 469)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataSet11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataSet21, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SqlSelectCommand1 As SqlClient.SqlCommand
+    Friend WithEvents SqlConnection1 As SqlClient.SqlConnection
+    Friend WithEvents SqlInsertCommand1 As SqlClient.SqlCommand
+    Friend WithEvents SqlDataAdapter1 As SqlClient.SqlDataAdapter
+    Friend WithEvents DataSet11 As DataSet1
+    Friend WithEvents DataSet21 As DataSet2
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents IdDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents AnyoDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents AnuncianteDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+End Class
---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' En la carga del formulario se carga el conjunto de datos
+        DataSet21.Clear()
+        SqlDataAdapter1.Fill(DataSet21)
+
+        ' A partir de aquí el usuario utiliza el control DataGridView para leer,
+        ' eliminar, insertar o modificar datos.
+        ' Los datos trabajan sin conexión
+
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+        ' Al cerrar el formulario se verifica si el usuario ha realizado algún cambio
+        ' El método HasChanges no devuelve True cuando haya cambios pendientes de enviar
+        ' al origen de datos
+        If (DataSet21.HasChanges()) Then
+            ' El adapatador de datos ya sabe que sentencia SQL debe utilizar para 
+            ' modificar los cambios
+            SqlDataAdapter1.Update(DataSet21)
+            MessageBox.Show("No han habido cambios en los datos.")
+        Else
+            MessageBox.Show("Cambios pasados al origen de datos.")
+        End If
+
+    End Sub
+
+End Class
